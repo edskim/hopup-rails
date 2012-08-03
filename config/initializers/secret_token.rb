49af29658297c8f90c21b82ef0307b7d0b66e52c
Eddie <edskim@gmail.com>
2012-08-03 12:20:34 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GpsMsg::Application.config.secret_token = '5bbb0ce1ff597c9362fdbadab22ef08828bb82dc8752109805af174a7a09f3cab5d484afd857541a13c82457c32feb988793819378f8a0976c64c928b4cef187'
