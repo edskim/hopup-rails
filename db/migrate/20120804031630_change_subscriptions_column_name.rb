83b032732c6ed067967ecf47fd3e3fd6d6fa1291
Eddie <edskim@gmail.com>
2012-08-05 23:34:18 -0700

    Remove unneeded migrations

--- db/migrate/20120804031630_change_subscriptions_column_name.rb
+++
@@ -1,9 +0,0 @@
-class ChangeSubscriptionsColumnName < ActiveRecord::Migration
-  def up
-    rename_column :topics, :user_id, :creator_id
-  end
-
-  def down
-    rename_column :topics, :creator_id, :user_id
-  end
-end
--- db/migrate/20120804033819_change_subscription_column_name.rb
+++
@@ -1,9 +0,0 @@
-class ChangeSubscriptionColumnName < ActiveRecord::Migration
-  def up
-    rename_column :subscriptions, :creator_id, :user_id
-  end
-
-  def down
-    rename_column :subscriptions, :user_id, :creator_id
-  end
-end
